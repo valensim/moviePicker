7367890e6661228427b83e3f96ab3c09a7b89d68
Simon Valenta <simon.valenta@emplifi.io>
2025-06-27 17:50:20 +0200

    darmoth to ironic

--- app.js
+++ app.js
@@ -10,6 +10,7 @@ const {
   REST,
   Routes,
 } = require("discord.js");
+const { ironic } = require("ironicase");
 
 const BOT_TOKEN = process.env.CLIENT_TOKEN;
 process.env.NODE_TLS_REJECT_UNAUTHORIZED = "0";
@@ -80,6 +81,42 @@ client.on(Events.InteractionCreate, async (interaction) => {
       });
     }
   }
+});
+
+const darmothNames = ['zlakurwica', 'milovník úvěrů', 'darmoth'];
+// Handle replies to specific users
+client.on(Events.MessageCreate, async (message) => {
+  // Ignore messages from bots (including itself)
+  if (message.author.bot) return;
+
+  const username = message.author.username.toLowerCase();
+
+  if (username === 'zlakurwica') {
+    const ironicMessage = ironic(message.content);
+    const channel = message.channel;
+    await message.delete();
+    const name = darmothNames[Math.floor(Math.random() * darmothNames.length)];
+    await channel.send( name + ' tried to yap: \n' + ironicMessage);
+    return;
+  }
+
+  return;
+  
+  // Check if this user has custom responses
+  // for (const [user, responses] of Object.entries(userResponses)) {
+  //   if (username.includes(user.toLowerCase())) {
+  //     // Pick a random response from the user's response array
+  //     const randomResponse = responses[Math.floor(Math.random() * responses.length)];
+      
+  //     try {
+  //       await message.reply(randomResponse);
+  //       console.log(`Replied to ${message.author.username}: "${randomResponse}"`);
+  //     } catch (error) {
+  //       console.error('Error replying to user message:', error);
+  //     }
+  //     break; // Only respond once per message
+  //   }
+  // }
 });
 
 client.login(BOT_TOKEN);
